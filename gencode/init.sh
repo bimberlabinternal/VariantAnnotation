3985a46b58d832685510e8e32486eaf614b5d953
bbimber <bbimber@gmail.com>
2022-11-07 13:05:58 -0800

    Add missing config file

--- gencode/init.sh
+++ gencode/init.sh
@@ -23,6 +23,7 @@ if [[ `isProcessingCompleted` == 0 ]];then
 	wget -O fixGencodeOrdering.py https://raw.githubusercontent.com/broadinstitute/gatk/master/scripts/funcotator/data_sources/fixGencodeOrdering.py
 
 	chmod +x getGencode.sh
+	chmod +x fixGencodeOrdering.sh
 	bash getGencode.sh
 	
 	# Normalize file locations:
