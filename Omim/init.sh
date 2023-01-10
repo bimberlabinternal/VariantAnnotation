13d4dd3b92a7f89e20f4177a0cb374b5db3f723a
bbimber <bbimber@gmail.com>
2023-01-09 17:19:57 -0800

    Update OMIM source

--- Omim/init.sh
+++ Omim/init.sh
@@ -18,23 +18,14 @@ if [[ `isProcessingCompleted` == 0 ]];then
 	downloadSourceFile $URL $TEMP_FILE
 
 	{
-	echo '##fileformat=bed';
-	echo '##INFO=<ID=OMIM,Number=A,Type=Float,Description="This is the omim score">';
-	echo '#CHROM	START	END	REF	SCORE';
+	echo '#CHROM	START	END	PMID';
 	## NOTE: BED files are 0-based, open coordinates:
-	cat $TEMP_FILE | grep -v '#' | awk -v OFS='\t' ' { print $1, $2, $3, $6 } ';
-	#cat $TEMP_FILE | grep -v '#' | awk -v OFS='\t' ' { print $1, $2, ".", ".", ".", ".", "PASS", "OMIM="$6 } ';
-	} | awk 'NR == 3; NR > 3 {print $0 | "sort -k1,1 -k2,2n -k6,6"}' | bgzip --threads $N_THREADS > $OUTFILE
-	
-	#ensureBedOrVcfSorted $OUTFILE
+	cat $TEMP_FILE | grep -v '#' | awk -F'\t' -v OFS='\t' ' { print $1-1, $2, $3, $6 } ' | sort --parallel $N_THREADS -V -k1,1 -k2,2n -k3,3n;
+	} | bgzip --threads $N_THREADS > $OUTFILE
+
 	ensureIndexed $OUTFILE
 	rm $TEMP_FILE
 	touch $DONE_FILE
 fi
 
 createConfigFileForBed $NAME $GENOME $OUTFILE
-
-#should this be a bed file bc there is no ref and alt data?
-## NOTE: BED files are 0-based, open coordinates:
-#cat $TEMP | awk -v OFS='\t' ' { print $1, $2, $3, $6 } ' > omim.bed
-#| sort -k1,1 -k2,2n -k6,6
