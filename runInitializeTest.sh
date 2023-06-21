8a48665c1b0199216d8f328721cc76799c08aca1
bbimber <bbimber@gmail.com>
2023-06-20 17:18:45 -0700

    Expand test to run tool

--- runInitializeTest.sh
+++ runInitializeTest.sh
@@ -7,17 +7,26 @@ export SKIP_LARGE_SOURCES=1
 
 bash initializeAll.sh
 
-#VCF=simpleTest.vcf
-#DATA_SOURCE=.
-#OUTPUT=testFuncotator.vcf
-#FASTA=
-#
-#gatk IndexFeatureFile -I $VCF
-#
-#gatk Funcotator \
-#	-R $FASTA
-#	--variant $VCF \
-#	--ref-version hg19 \
-#	--data-sources-path $DATA_SOURCE \
-#	--output $OUTPUT \
-#	--output-file-format MAF
+VCF=simpleTest.vcf
+DATA_SOURCE=.
+OUTPUT=testFuncotator.vcf
+
+FASTA=genome.fasta
+
+wget -O ${FASTA}.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.25_GRCh37.p13/GCF_000001405.25_GRCh37.p13_genomic.fna.gz
+gunzip ${FASTA}.gz
+
+samtools faidx -R $FASTA
+gatk CreateSequenceDictionary $FASTA
+
+gatk IndexFeatureFile -I $VCF
+
+gatk Funcotator \
+	-R $FASTA
+	--variant $VCF \
+	--ref-version hg19 \
+	--data-sources-path $DATA_SOURCE \
+	--output $OUTPUT \
+	--output-file-format MAF
+
+rm genome.fasta
