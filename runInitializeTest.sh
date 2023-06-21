ce357cabaeca15e4183ae755e12fdd2311aed9e8
bbimber <bbimber@gmail.com>
2023-06-20 17:34:34 -0700

    Fix test syntax

--- runInitializeTest.sh
+++ runInitializeTest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # This script can be used to sanity-check the data source downloads
 export DOWNLOAD_LINE_LIMIT=1000
 export FORCE_REPROCESS=0
@@ -16,7 +18,7 @@ FASTA=genome.fasta
 wget -O ${FASTA}.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.25_GRCh37.p13/GCF_000001405.25_GRCh37.p13_genomic.fna.gz
 gunzip ${FASTA}.gz
 
-samtools faidx -R $FASTA
+samtools faidx $FASTA
 gatk CreateSequenceDictionary -R $FASTA
 
 gatk IndexFeatureFile -I $VCF
