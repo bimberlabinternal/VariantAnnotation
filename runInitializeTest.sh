a9aeca72d864c04efe631d847df2a1bc1eda3fa7
bbimber <bbimber@gmail.com>
2023-06-21 15:21:13 -0700

    Remove FASTA download

--- runInitializeTest.sh
+++ runInitializeTest.sh
@@ -16,20 +16,21 @@ OUTPUT=testFuncotator.vcf
 
 FASTA=Homo_sapiens.GRCh37.75.fasta
 
-wget -O ${FASTA}.gz https://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.75.dna_sm.toplevel.fa.gz
-gunzip ${FASTA}.gz
-
-samtools faidx $FASTA
-gatk CreateSequenceDictionary -R $FASTA
-
-gatk IndexFeatureFile -I $VCF
-
-gatk Funcotator \
-	-R $FASTA \
-	--variant $VCF \
-	--ref-version hg19 \
-	--data-sources-path $DATA_SOURCE \
-	--output $OUTPUT \
-	--output-file-format MAF
-
-rm $FASTA
+# NOTE: this is too large to work on github's agents:
+#wget -O ${FASTA}.gz https://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.75.dna_sm.toplevel.fa.gz
+#gunzip ${FASTA}.gz
+#
+#samtools faidx $FASTA
+#gatk CreateSequenceDictionary -R $FASTA
+#
+#gatk IndexFeatureFile -I $VCF
+#
+#gatk Funcotator \
+#	-R $FASTA \
+#	--variant $VCF \
+#	--ref-version hg19 \
+#	--data-sources-path $DATA_SOURCE \
+#	--output $OUTPUT \
+#	--output-file-format MAF
+#
+#rm $FASTA
