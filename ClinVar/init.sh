965f996162fead111a44284ef8987ec70f6df60f
bbimber <bbimber@gmail.com>
2023-10-26 08:15:23 -0700

    Skip ClinVar variant with IUPAC bases

--- ClinVar/init.sh
+++ ClinVar/init.sh
@@ -19,14 +19,20 @@ NAME=ClinVar
 if [[ `isProcessingCompleted` == 0 ]];then
 	ensureGenomeFolderExists $GENOME
 	downloadSourceFile $URL $TEMP_FILE
-	downloadSourceFile ${URL}.tbi ${TEMP_FILE}.tbi
+	
+	# Position 11:15163 contains IUPAC bases, which HTSJDK does not allow:
+	TEMP_FILE2=clinvar_20230702.ss.vcf.gz
+	zcat $TEMP_FILE | awk -V OFS='\t' ' $5 != "YT" ' | bgzip -f > $TEMP_FILE2
+	tabix -p vcf $TEMP_FILE2
 
-	cp $TEMP_FILE $OUTFILE
-	cp ${TEMP_FILE}.tbi ${OUTFILE}.tbi
+	cp $TEMP_FILE2 $OUTFILE
+	cp ${TEMP_FILE2}.tbi ${OUTFILE}.tbi
 	
 	rm -Rf $TEMP_FILE
-	rm -Rf ${TEMP_FILE}.tbi 
 
+	rm -Rf $TEMP_FILE2
+	rm -Rf ${TEMP_FILE2}.tbi 
+	
 	touch $DONE_FILE
 fi
 
